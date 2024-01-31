0272b8f47d922a9f9e737e9ed543af320a2cb38a
Marwan Samamreh <119597263+MarwanSamamreh@users.noreply.github.com>
2024-01-31 22:41:43 +0200

    Delete src directory

--- src/routes/brands.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const brandsRoutes = express.Router()
-import * as brandsController from "../controllers/brandsContraller"
-
-brandsRoutes.get("/",brandsController.getALLBrands);
-
-export default brandsRoutes;
--- src/routes/categories.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as categoriesController from "../controllers/categoriesController";
-const router = express.Router()
-
-router.get("/",categoriesController.getAllCategories);
-router.get("/:id",categoriesController.getOneCategoryById)
-
-export default router;
